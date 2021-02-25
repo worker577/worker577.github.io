eada3358569e9f994b5b11b3e8bcd41ba8de64a0
worker577 <59944771+worker577@users.noreply.github.com>
2021-02-25 19:21:11 +0800

    Add files via upload

---
+++ recscreen/assets/js/scripts.js
@@ -0,0 +1,14 @@
+
+jQuery(document).ready(function() {
+	
+    /*
+        Fullscreen background
+    */
+    $.backstretch("assets/img/backgrounds/1.jpg");
+    
+    $('.login-form').on('submit', function(e) {
+    	alert('Thanks for your feedback')
+    });
+    
+    
+});
