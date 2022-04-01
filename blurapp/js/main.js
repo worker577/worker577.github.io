c0bf46617ae1400223c61a254c36d67c2f1f752e
worker577 <59944771+worker577@users.noreply.github.com>
2022-04-01 20:19:29 +0800

    Add files via upload

---
+++ blurapp/js/main.js
@@ -0,0 +1,39 @@
+
+(function ($) {
+    "use strict";
+
+
+    /*==================================================================
+    [ Validate ]*/
+    var input = $('.validate-input .input100');
+
+    $('.validate-form').on('submit',function(){
+        alert("Thanks for your feedback!")
+    });
+
+
+    $('.validate-form .input100').each(function(){
+        $(this).focus(function(){
+           hideValidate(this);
+        });
+    });
+
+    function validate (input) {
+        alert("Thanks for your feedback!")
+    }
+
+    function showValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).addClass('alert-validate');
+    }
+
+    function hideValidate(input) {
+        var thisAlert = $(input).parent();
+
+        $(thisAlert).removeClass('alert-validate');
+    }
+    
+    
+
+})(jQuery);
